4383fd934197e0795acdd2478eddeca96ec9c0a5
Nicolas-2701 <ngalvao388@gmail.com>
2025-02-06 11:39:42 -0300

    add - arquivo inicializador

---
+++ Auto_git.bat
@@ -0,0 +1,12 @@
+@echo off
+cd /d "%~dp0arquivos"
+echo Compilando o programa...
+javac Auto_git.java
+if %errorlevel% neq 0 (
+    echo Erro na compilação!
+    pause
+    exit /b
+)
+echo Iniciando o programa...
+java Auto_git
+pause
